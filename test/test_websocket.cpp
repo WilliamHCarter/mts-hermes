06d5eaceee71a5a2c9cdd156ae88889f855a8676
Will Carter <41378465+WilliamHCarter@users.noreply.github.com>
2025-07-28 23:58:07 -0400

    simple ws testing and documentation

--- test/test_websocket.cpp
+++ test/test_websocket.cpp
@@ -1,6 +1,7 @@
 #include "../src/websocket.cpp"
 #include <gtest/gtest.h>
 
+// Verify websocket client construction and connect callback setting logic.
 TEST(IXWebSocketAdapterTest, CanCreateWebSocketClient) {
     IXWebSocketAdapter ws;
 
@@ -10,4 +11,43 @@ TEST(IXWebSocketAdapterTest, CanCreateWebSocketClient) {
     });
 
     EXPECT_FALSE(onConnectCalled);
+}
+
+// Ensure setting a connect callback overwrites any previously assigned one.
+TEST(IXWebSocketAdapterTest, CanOverwriteCallbacks) {
+    IXWebSocketAdapter ws;
+
+    bool firstCallback = false;
+    bool secondCallback = false;
+
+    ws.setOnConnect([&firstCallback]() {
+        firstCallback = true;
+    });
+
+    ws.setOnConnect([&secondCallback]() {
+        secondCallback = true;
+    });
+
+    EXPECT_FALSE(firstCallback);
+    EXPECT_FALSE(secondCallback);
+}
+
+// Confirm that two websocket instances maintain independent state and callback registrations.
+TEST(IXWebSocketAdapterTest, MultipleInstancesAreIndependent) {
+    IXWebSocketAdapter ws1;
+    IXWebSocketAdapter ws2;
+
+    bool ws1Connected = false;
+    bool ws2Connected = false;
+
+    ws1.setOnConnect([&ws1Connected]() {
+        ws1Connected = true;
+    });
+
+    ws2.setOnConnect([&ws2Connected]() {
+        ws2Connected = true;
+    });
+
+    EXPECT_FALSE(ws1Connected);
+    EXPECT_FALSE(ws2Connected);
 }
