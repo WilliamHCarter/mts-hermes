d46f214398f4944267959e39819c48aa8d89f8c1
Will Carter <41378465+WilliamHCarter@users.noreply.github.com>
2025-08-03 23:58:25 -0400

    better error setup

--- src/websocket.cpp
+++ src/websocket.cpp
@@ -9,21 +9,22 @@ public:
     using MessageCallback = std::function<void(const std::string&)>;
     using ConnectCallback = std::function<void()>;
     using DisconnectCallback = std::function<void()>;
+    using ErrorCallback = std::function<void(const std::string& reason, int code)>;
 
     virtual ~IWebSocketClient() = default;
-
     virtual void connect(const std::string& url) = 0;
     virtual void send(const std::string& message) = 0;
     virtual void close() = 0;
-
     virtual void setOnMessage(MessageCallback callback) = 0;
     virtual void setOnConnect(ConnectCallback callback) = 0;
     virtual void setOnDisconnect(DisconnectCallback callback) = 0;
+    virtual void setOnError(ErrorCallback callback) = 0;
 };
 
 class IXWebSocketAdapter : public IWebSocketClient {
 public:
     IXWebSocketAdapter() = default;
+
     ~IXWebSocketAdapter() override {
         ws.stop();
     }
@@ -35,25 +36,21 @@ public:
                 case ix::WebSocketMessageType::Open:
                     if (onConnect) onConnect();
                     break;
-
                 case ix::WebSocketMessageType::Close:
                     if (onDisconnect) onDisconnect();
                     break;
-
                 case ix::WebSocketMessageType::Message:
                     if (onMessage) onMessage(msg->str);
                     break;
-
                 case ix::WebSocketMessageType::Error:
-                    std::cout << "WebSocket Error: " << msg->errorInfo.reason
-                                << " (code: " << msg->errorInfo.retries << ")" << std::endl;
-                    break; //TODO: maybe we try using c++ result stuff idk.
-
+                    if (onError) {
+                        onError(msg->errorInfo.reason, msg->errorInfo.retries);
+                    }
+                    break;
                 default:
                     break;
             }
         });
-
         ws.start();
     }
 
@@ -65,13 +62,26 @@ public:
         ws.stop();
     }
 
-    void setOnMessage(MessageCallback callback) override { onMessage = std::move(callback); }
-    void setOnConnect(ConnectCallback callback) override { onConnect = std::move(callback); }
-    void setOnDisconnect(DisconnectCallback callback) override { onDisconnect = std::move(callback); }
+    void setOnMessage(MessageCallback callback) override {
+        onMessage = std::move(callback);
+    }
+
+    void setOnConnect(ConnectCallback callback) override {
+        onConnect = std::move(callback);
+    }
+
+    void setOnDisconnect(DisconnectCallback callback) override {
+        onDisconnect = std::move(callback);
+    }
+
+    void setOnError(ErrorCallback callback) override {
+        onError = std::move(callback);
+    }
 
 private:
     ix::WebSocket ws;
     MessageCallback onMessage;
     ConnectCallback onConnect;
     DisconnectCallback onDisconnect;
+    ErrorCallback onError;
 };
